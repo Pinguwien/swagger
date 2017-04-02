88339f308402517ccbdd8c3cca48863ce7ed8b95
Thomas Junk <lilith2k3@archlinux.us>
2017-04-02 17:46:49 +0200

    renamed swaggerfile

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ logging.basicConfig(level=logging.INFO)
 
 if __name__ == '__main__':
     app = connexion.App(__name__, specification_dir='swagger/')
-    app.add_api('efa-api.yaml', arguments={'title': 'eFa'},
+    app.add_api('swagger.yaml', arguments={'title': 'eFa'},
                 resolver=RestyResolver('api'))
     app.run(port=8000)
