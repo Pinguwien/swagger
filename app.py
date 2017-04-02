b99212ce034153d2954f3fb3ea718fd04e408226
Thomas Junk <lilith2k3@archlinux.us>
2017-04-02 18:00:46 +0200

    package.json added

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from connexion.resolver import RestyResolver
 logging.basicConfig(level=logging.INFO)
 
 if __name__ == '__main__':
-    app = connexion.App(__name__, specification_dir='swagger/')
+    app = connexion.App(__name__, specification_dir='api/swagger/')
     app.add_api('swagger.yaml', arguments={'title': 'eFa'},
                 resolver=RestyResolver('api'))
     app.run(port=8000)
