67c91e47a7e0a867619a04e249eb3231b1e6b060
Thomas Junk <lilith2k3@archlinux.us>
2017-04-02 17:23:04 +0200

    GET by id, POST and DELETE implemented

--- api/participants.py
+++ api/participants.py
@@ -1,10 +1,13 @@
 import tinydb
-import ujson
+from connexion import NoContent
+
+counter = 3
+prefix = "EFA-0815"
 
 db = tinydb.TinyDB('db.json')
 db.purge()
 db.insert_multiple([{
-    'personalid': 'EFA-0815-0000001',
+    'personalid': 'EFA-0815-000001',
     'firstname': 'Irina',
     'lastname': 'Sarzew',
     'participations': [
@@ -14,14 +17,13 @@ db.insert_multiple([{
             'type': 'CaseManagement'
         }
     ],
-    'outputindicators': [
-        {
-            'CO01': True,
-            'CO02': False
-        }
-    ]},
+    'outputindicators': {
+        'CO01': True,
+        'CO02': False
+    }
+},
     {
-    'personalid': 'EFA-0815-0000002',
+    'personalid': 'EFA-0815-000002',
     'firstname': 'Max',
     'lastname': 'Mustermann',
     'participations': [
@@ -31,12 +33,10 @@ db.insert_multiple([{
             'type': 'Microproject'
         }
     ],
-    'outputindicators': [
-        {
-            'CO01': True,
-            'CO02': False
-        }
-    ]
+    'outputindicators': {
+        'CO01': True,
+        'CO02': False
+    }
 }])
 
 
@@ -46,4 +46,28 @@ def search():
 
 def get(id):
     Participants = tinydb.Query()
-    return ujson.dumps(db.search(Participants.personalid == id))
+    result = db.search(Participants.personalid == id)
+    if not result:
+        return NoContent, 404
+    return result
+
+
+def post(participant):
+    global counter
+    personalid = "{}".format(counter).zfill(6)
+    p = {
+        'firstname': participant.get("firstname"),
+        'lastname': participant.get("lastname"),
+        'personalid': "{}{}".format(prefix, personalid),
+        'participations': [],
+        'outputindicators': {}
+    }
+    db.insert(p)
+    counter = counter+1
+    return p, 201
+
+
+def delete(id):
+    Participants = tinydb.Query()
+    db.remove(Participants.personalid == id)
+    return NoContent, 404
